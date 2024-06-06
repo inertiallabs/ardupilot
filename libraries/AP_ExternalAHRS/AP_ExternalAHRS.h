072d1306aeed5ec943461cdba03ec1e320648f92
inertialjohn <dn@inertiallabs.com>
2024-06-06 19:32:48 +0300

    Renamed the second bit of the EAHRS_OPTIONS parameter

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
@@ -179,7 +179,7 @@ public:
 
     enum class OPTIONS {
         VN_UNCOMP_IMU = (1U << 0),
-        ILAB_ENABLE_CLB = (1U << 1), // Enable InertialLabs INS compass, accelerometer and gyro calibration
+        ILAB_ENABLE_CLB = (1U << 1), // Disable InertialLabs INS compass, accelerometer and gyro calibration
         ILAB_USE_BARO_ALT = (1U << 2), // Use InertialLabs INS baro altitude and vertical velocity instead of calculated by Ardupilot
         ILAB_USE_AIRSPEED = (1U << 3), // Use InertialLabs INS airspeed and wind estimation instead of calculated by Ardupilot
     };
