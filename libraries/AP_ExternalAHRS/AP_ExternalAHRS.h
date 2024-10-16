c7cba341895fee1e7b460359a35f418171a2c2a5
inertialjohn <dn@inertiallabs.com>
2024-10-16 13:14:37 +0300

    AP_ExternalAHRS: Added the use of IL INS wind estimation in ArduPilot subsystems

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
@@ -136,6 +136,7 @@ public:
     bool get_accel(Vector3f &accel);
     void send_status_report(class GCS_MAVLINK &link) const;
     bool get_variances(float &velVar, float &posVar, float &hgtVar, Vector3f &magVar, float &tasVar) const;
+    bool get_estimate_wind(Vector3f &wind) const;
 
     // update backend
     void update();
