2d6cca55677077065f4aea96ed2202cc03d175cd
inertialjohn <dn@inertiallabs.com>
2024-10-18 21:09:05 +0300

    Changed the priority of IL INS status messages for sending to the GCS

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS_InertialLabs_message_list.h
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS_InertialLabs_message_list.h
@@ -111,17 +111,17 @@ const ILStatusMessage IL_usw_msg[] = {
 	{ GNSS_FAIL,              MAV_SEVERITY_CRITICAL, "GNSS receiver failure",                 "GNSS receiver is OK"              },
 	{ VOLTAGE_LOW,            MAV_SEVERITY_WARNING,  "Low input voltage",                     "Input voltage is in range"        },
 	{ VOLTAGE_HIGH,           MAV_SEVERITY_WARNING,  "High input voltage",                    "Input voltage is in range"        },
-	{ GYRO_X_RATE_HIGH,       MAV_SEVERITY_WARNING,  "Y-axis angular rate is exceeded",       "Y-axis angular rate is in range"  },
-	{ GYRO_Y_RATE_HIGH,       MAV_SEVERITY_WARNING,  "X-axis angular rate is exceeded",       "X-axis angular rate is in range"  },
-	{ GYRO_Z_RATE_HIGH,       MAV_SEVERITY_WARNING,  "Z-axis angular rate is exceeded",       "Z-axis angular rate is in range"  },
-	{ MAG_FIELD_HIGH,         MAV_SEVERITY_WARNING,  "Large total magnetic field",            "Total magnetic field is in range" },
-	{ TEMP_RANGE_ERR,         MAV_SEVERITY_WARNING,  "Temperature is out of range",           "Temperature is in range"          }
+	{ GYRO_X_RATE_HIGH,       MAV_SEVERITY_INFO,     "Y-axis angular rate is exceeded",       "Y-axis angular rate is in range"  },
+	{ GYRO_Y_RATE_HIGH,       MAV_SEVERITY_INFO,     "X-axis angular rate is exceeded",       "X-axis angular rate is in range"  },
+	{ GYRO_Z_RATE_HIGH,       MAV_SEVERITY_INFO,     "Z-axis angular rate is exceeded",       "Z-axis angular rate is in range"  },
+	{ MAG_FIELD_HIGH,         MAV_SEVERITY_INFO,     "Large total magnetic field",            "Total magnetic field is in range" },
+	{ TEMP_RANGE_ERR,         MAV_SEVERITY_INFO,     "Temperature is out of range",           "Temperature is in range"          }
 };
 
 const ILStatusMessage IL_usw2_msg[] = {
-	{ ACCEL_X_HIGH,            MAV_SEVERITY_WARNING,  "Y-axis acceleration is out of range",     "Y-axis acceleration is in range"     },
-	{ ACCEL_Y_HIGH,            MAV_SEVERITY_WARNING,  "X-axis acceleration is out of range",     "X-axis acceleration is in range"     },
-	{ ACCEL_Z_HIGH,            MAV_SEVERITY_WARNING,  "Z-axis acceleration is out of range",     "Z-axis acceleration is in range"     },
+	{ ACCEL_X_HIGH,            MAV_SEVERITY_INFO,     "Y-axis acceleration is out of range",     "Y-axis acceleration is in range"     },
+	{ ACCEL_Y_HIGH,            MAV_SEVERITY_INFO,     "X-axis acceleration is out of range",     "X-axis acceleration is in range"     },
+	{ ACCEL_Z_HIGH,            MAV_SEVERITY_INFO,     "Z-axis acceleration is out of range",     "Z-axis acceleration is in range"     },
 	{ ADU_BARO_FAIL,           MAV_SEVERITY_CRITICAL, "Baro altimeter failure",                  "Baro altimeter is OK"                },
 	{ ADU_DIFF_PRESS_FAIL,     MAV_SEVERITY_CRITICAL, "Diff. pressure sensor failure",           "Diff. pressure sensor is OK"         },
 	{ MAG_AUTO_CAL_2D_RUNTIME, MAV_SEVERITY_INFO,     "Automatic 2D calibration is in progress", "Automatic 2D calibration is stopped" },
@@ -134,8 +134,8 @@ const ILStatusMessage IL_usw2_msg[] = {
 const ILStatusMessage IL_adu_msg[] = {
 	{ BARO_INIT_FAIL,           MAV_SEVERITY_WARNING, "Static pressure sensor unsuccessful initialization", "Static pressure sensor initialization successful"},
 	{ DIFF_PRESS_INIT_FAIL,     MAV_SEVERITY_WARNING, "Diff. pressure sensor unsuccessful initialization",  "Diff. pressure sensor initialization successful"},
-	{ BARO_RANGE_ERR,           MAV_SEVERITY_WARNING, "Static pressure is out of range",                    "Static pressure is in range"      },
-	{ DIFF_PRESS_RANGE_ERR,     MAV_SEVERITY_WARNING, "Diff. pressure is out of range",                     "Diff. pressure is in range"       },
+	{ BARO_RANGE_ERR,           MAV_SEVERITY_INFO,    "Static pressure is out of range",                    "Static pressure is in range"      },
+	{ DIFF_PRESS_RANGE_ERR,     MAV_SEVERITY_INFO,    "Diff. pressure is out of range",                     "Diff. pressure is in range"       },
 	{ BARO_ALT_FAIL,            MAV_SEVERITY_WARNING, "Pressure altitude is incorrect",                     "Pressure altitude is correct"     },
 	{ AIRSPEED_FAIL,            MAV_SEVERITY_WARNING, "Air speed is incorrect",                             "Air speed is correct"             },
 	{ AIRSPEED_BELOW_THRESHOLD, MAV_SEVERITY_INFO,    "Air speed is below the threshold",                   "Air speed is above the threshold" }
