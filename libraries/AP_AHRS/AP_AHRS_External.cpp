a4b479e13a87ba49fc85aa79a1847994f167ce83
Valentin Bugrov <vladbvnsk@gmail.com>
2025-06-11 15:38:10 -0400

    AP_AHRS: Add wind estimate getting from EAHRS

--- libraries/AP_AHRS/AP_AHRS_External.cpp
+++ libraries/AP_AHRS/AP_AHRS_External.cpp
@@ -136,4 +136,9 @@ void AP_AHRS_External::get_control_limits(float &ekfGndSpdLimit, float &ekfNavVe
     ekfNavVelGainScaler = 0.5;
 }
 
+bool AP_AHRS_External::wind_estimate(Vector3f &ret) const
+{
+    return AP::externalAHRS().get_estimate_wind(ret);
+}
+
 #endif
